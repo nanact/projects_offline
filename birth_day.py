5bc36ea095804a946ee0dbb2c6a88711c44895d9
nanact <144712705+nanact@users.noreply.github.com>
2024-11-10 14:44:35 +0000

    Create birth_day.py

---
+++ birth_day.py
@@ -0,0 +1,39 @@
+bday1 = int(input("what is the day of your birth day (number format)"))
+
+bmonth1 = int(input("what is the month of your birth day (number format)"))
+
+byear1 = int(input("what is the year of your birth day"))
+
+bday2 = int(input("what is the day of your birth day (number format)"))
+
+bmonth2 = int(input("what is the month of your birth day (number format)"))
+
+byear2 = int(input("what is the year of your birth day"))
+
+bday3 = int(input("what is the day of your birth day (number format)"))
+
+bmonth3 = int(input("what is the month of your birth day (number format)"))
+
+byear3 = int(input("what is the year of your birth day"))
+
+bday4 = int(input("what is the day of your birth day (number format)"))
+
+bmonth4 = int(input("what is the month of your birth day (number format)"))
+
+byear4 = int(input("what is the year of your birth day"))
+
+bday5 = int(input("what is the day of your birth day (number format)"))
+
+bmonth5 = int(input("what is the month of your birth day (number format)"))
+
+byear5 = int(input("what is the year of your birth day"))
+
+print("first birthday is",str(bday1),"/",str(bmonth1),"/",str(byear1))
+
+print("second birthday is",str(bday2),"/",str(bmonth2),"/",str(byear2))
+
+print("third birthday is",str(bday3),"/",str(bmonth3),"/",str(byear3))
+
+print("fourth birthday is",str(bday4),"/",str(bmonth4),"/",str(byear4))
+
+print("fifth birthday is",str(bday5),"/",str(bmonth5),"/",str(byear5))
