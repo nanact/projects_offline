5169b4fdb18c24417e568854f2884b6e297a5896
nanact <144712705+nanact@users.noreply.github.com>
2024-11-10 15:44:18 +0000

    Create square.py

---
+++ square.py
@@ -0,0 +1,28 @@
+import turtle
+
+turtle.Screen()
+
+turtle.bgcolor("blue")
+
+turtle.title("square")
+
+square = turtle.Turtle()
+
+square.pensize(40)
+
+square.forward(90) 
+square.left(90) 
+ 
+
+square.forward(90) 
+square.left(90) 
+ 
+
+square.forward(90) 
+square.left(90) 
+ 
+
+square.forward(90) 
+square.left(90)
+
+turtle.done()
