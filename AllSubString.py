6961c8be29bab063baf5f1f13472dca31c266088
nanact <144712705+nanact@users.noreply.github.com>
2025-07-21 20:12:27 -0200

    Create AllSubString.py

---
+++ AllSubString.py
@@ -0,0 +1,9 @@
+def all_substrings(s):
+    substrings = []
+    for i in range(len(s)):
+        for j in range(i + 1, len(s) + 1):
+            substrings.append(s[i:j])
+    return substrings
+
+s = "cat"
+print("All substrings:", all_substrings(s))
